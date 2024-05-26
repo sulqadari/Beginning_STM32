a1fa82134b43590e847cbf928e6a00c6c90e6b5d
Telman Islamov <sulqadari@gmail.com>
2024-05-26 23:32:30 +0300

    USART 0.4: firmware doesn't response.
    The main body, all functions and params seems like correct
    but there is no response from the MCU.
    
    The same issue with the reference implementation

--- 01_uart/src/main.c
+++ 01_uart/src/main.c
@@ -1,117 +1,172 @@
-#include "usart_utils.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
 
-static QueueHandle_t uart_txq;
-bool doPrintHint = true;
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/common/usart_common_all.h>
+
+static QueueHandle_t queue_TX, queue_RX;
+
+static bool inline
+isLower(int32_t ch)
+{
+	return (ch >= 'a' && ch <= 'z');
+}
+
+static bool inline
+isUpper(int32_t ch)
+{
+	return (ch >= 'A' && ch <= 'Z');
+}
+
+static bool inline
+toLower(int32_t ch)
+{
+	return ch + 32;
+}
+
+static bool inline
+toUpper(int32_t ch)
+{
+	return ch - 32;
+}
 
 static void
-gpio_setup(void)
+init_clock(void)
 {
-	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
-
-	rcc_periph_clock_enable(RCC_GPIOC);
-	// Setup LED
-	gpio_set_mode(
-		GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-		GPIO_CNF_OUTPUT_PUSHPULL, GPIO13
-	);
-
 	rcc_periph_clock_enable(RCC_GPIOA);
-	// Setup USART1 TX
-	gpio_set_mode(
-		GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-		GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX
-	);
-
-	// set USART1 CTS as output in alternate push-pull mode with speed 50 MHz
-	gpio_set_mode(
-		GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-		GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO11
-	);
-
-	// Setup USART RX
-	gpio_set_mode(
-		GPIOA, GPIO_MODE_INPUT,
-		GPIO_CNF_INPUT_FLOAT, GPIO_USART1_RX
-	);
-
-	// set USART1 RTS as input in floating mode
-	gpio_set_mode(
-		GPIOA, GPIO_MODE_INPUT,
-		GPIO_CNF_INPUT_FLOAT, GPIO12
-	);
-
-}
-
-static int32_t
-uart_setup(void)
-{
-	if (uart_open(115200, "8N1", "rw", 1, 1) != 0)
-		return (-1);
-	
-	uart_txq = xQueueCreate(256, sizeof(char));
-	return (0);
+	rcc_periph_clock_enable(RCC_GPIOC);
+	rcc_periph_clock_enable(RCC_USART1);
 }
 
 static void
-task_uart(void* args __attribute__((unused)))
+init_LED(void)
 {
-	int32_t fetched;
-	char kbuf[256], ch;
+	gpio_set_mode(
+		GPIOC,
+		GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_PUSHPULL,
+		GPIO13
+	);
 
-	uart_puts("\r\n***FunTerm***\r\n");
-	for (;;) {
+	gpio_set(GPIOC, GPIO13);
+}
 
-		if ((fetched = uart_getc_nb()) != -1) {
-			
-			uart_puts(">>>: ");
-			ch = (char)fetched;
+static void
+init_uart(void)
+{
+	queue_TX = xQueueCreate(256, sizeof(char));
+	queue_RX = xQueueCreate(256, sizeof(char));
 
-			if (ch != '\r' && ch != '\n') {
-				kbuf[0] = ch;
-				uart_putc(ch);
-				uart_read_keystrokes((kbuf + 1), sizeof kbuf - 1);
-			} else {
-				// read the entire line.
-				uart_read_keystrokes(kbuf, sizeof kbuf);
-			}
+	nvic_enable_irq(NVIC_USART1_IRQ);
+	
+	gpio_set_mode(
+		GPIOA,
+		GPIO_MODE_OUTPUT_50_MHZ,
+		GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+		GPIO_USART1_TX
+	);
 
-			uart_puts("\n\r<<<'");
-			uart_puts(kbuf);
-			uart_puts("'\n\r");
-		}
+	gpio_set_mode(
+		GPIOA,
+		GPIO_MODE_INPUT,
+		GPIO_CNF_INPUT_FLOAT,
+		GPIO_USART1_RX
+	);
 
-		// Receive a char to be transmitted.
-		if (xQueueReceive(uart_txq, &ch, 10) == pdPASS) {
-			uart_putc(ch);
-			gpio_toggle(GPIOC, GPIO13);
-		}
+	usart_set_baudrate(USART1, 115200);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART1, USART_STOPBITS_1);
+	usart_set_mode(USART1, USART_MODE_TX_RX);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+
+	usart_enable_rx_interrupt(USART1);
+	usart_enable(USART1);
+}
+
+void
+USART1_IRQHandler(void)
+{
+	int32_t ch;
+	BaseType_t hpTask = pdFALSE;
+
+	while (((USART_SR(USART1) & USART_SR_RXNE) != 0)) {
+		ch = usart_recv(USART1);
+
+		xQueueSendToBackFromISR(queue_RX, &ch, &hpTask);
 	}
 }
 
-static inline void
-demo_print_string(const char* str)
+static void
+task_transmit(void* args __attribute((unused)))
 {
-	for ( ; *str; ++str)
-		xQueueSend(uart_txq, str, portMAX_DELAY);	// blocks when queue is full
+	int32_t ch;
+
+	for (;;) {
+		while (xQueueReceive(queue_TX, &ch, portMAX_DELAY) != pdPASS)
+			taskYIELD();
+		
+		while (!usart_get_flag(USART1, USART_SR_TXE))
+			taskYIELD();
+
+		usart_send(USART1, ch);
+	}
+}
+
+static int32_t
+read_char(void)
+{
+	int32_t ch;
+
+	while (xQueueReceive(queue_RX, &ch, 0) != pdPASS)
+		taskYIELD();
+	
+	gpio_toggle(GPIOC, GPIO13);
+	return ch;
 }
 
 static void
-task_blink(void* args __attribute__((unused)))
+write_char(int32_t ch)
+{
+_again:
+	while (xQueueSend(queue_TX, &ch, 0) != pdPASS)
+		taskYIELD();
+	
+	if (ch == '\n') {
+		ch = '\r';
+		goto _again;
+	}
+}
+
+static void
+task_main(void* args __attribute((unused)))
 {
 	for (;;) {
-		vTaskDelay(pdMS_TO_TICKS(2000));
-		gpio_toggle(GPIOC, GPIO13);
+		int32_t ch = read_char();
+
+		if (isLower(ch))
+			ch = toUpper(ch);
+		else if (isUpper(ch))
+			ch = toLower(ch);
+		
+		write_char(ch);
 	}
 }
 
 int
 main(void)
 {
-	gpio_setup();
-	uart_setup();
+	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 
-	xTaskCreate(task_uart, "UART_RTX", 100, NULL, configMAX_PRIORITIES - 1, NULL);
-	xTaskCreate(task_blink, "Blink", 100, NULL, configMAX_PRIORITIES - 1, NULL);
+	init_clock();
+	init_LED();
+	init_uart();
+
+	xTaskCreate(task_main, "MAIN", 100, NULL, configMAX_PRIORITIES - 1, NULL);
+	xTaskCreate(task_transmit, "TRANSMIT", 100, NULL, configMAX_PRIORITIES - 1, NULL);
 	
 	vTaskStartScheduler();
 	
